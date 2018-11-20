64e09d2d9b1ca13d32914c227860b1276d15a886
Hai Liang Wang <hailiang.hl.wang@gmail.com>
2018-11-20 19:17:18 +0800

    Enable v1 apis

---
+++ src/chatopera/sdk/Chatbot.php
@@ -0,0 +1,399 @@
+<?php
+/*	My super power is talk.
+ *	@Author: Hai Liang Wang
+ *  @Company: 北京华夏春松科技有限公司
+ *  All right reserved.
+ **/
+
+namespace Chatopera\SDK;
+use Exception;
+
+function generate($clientId, $secret, $method, $path)
+{
+    $timestamp = time();
+    $random = rand(1000000000, 9999999999);
+    $signature = hash_hmac('sha1', $clientId.$timestamp.$random.$method.$path, $secret);
+    $json =json_encode(array(
+        'appId' => $clientId,
+        'timestamp' => $timestamp,
+        'random' => $random,
+        'signature' => $signature
+    ));
+
+    return base64_encode($json);
+}
+
+
+class Chatbot {
+    private $baseUrl = "https://bot.chatopera.com";
+    private $clientId;
+    private $clientSecret;
+
+    public function __construct($clientId, $clientSecret)
+    {
+        $this->clientId = $clientId;
+        $this->clientSecret = $clientSecret;
+    }
+
+    /**
+     * 查看机器人详情
+     * @return mixed
+     */
+    public function detail(){
+        $service_path = '/api/v1/chatbot/' . $this->clientId;
+        $service_url = $this->baseUrl . $service_path;
+        $service_method = "GET";
+        $request = curl_init($service_url);
+        $token = generate($this->clientId, $this->clientSecret, $service_method, $service_path);
+
+        $headers = array(
+            "Content-Type: application/json",
+            "Authorization: $token",
+            "Accept: application/json"
+        );
+
+        curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($request, CURLOPT_CUSTOMREQUEST, $service_method);
+        curl_setopt($request, CURLOPT_HTTPHEADER, $headers);
+        $curl_response = curl_exec($request);
+
+        $http_code = curl_getinfo($request, CURLINFO_HTTP_CODE);
+
+        if($http_code != 200){
+            throw new HttpResponseException("Wrong Chatbot Response.");
+        }
+
+        return $this->purge(json_decode($curl_response, true));
+    }
+
+    /**
+     * 检索多轮对话
+     * @param $userId
+     * @param $textMessage
+     * @return mixed
+     * @throws Exception
+     */
+    public function conversation($userId, $textMessage){
+        $service_path = '/api/v1/chatbot/' . $this->clientId . '/conversation/query';
+        $service_url = $this->baseUrl . $service_path;
+        $service_method = "POST";
+        $request = curl_init($service_url);
+        $token = generate($this->clientId, $this->clientSecret, $service_method, $service_path);
+
+        $data = json_encode(array(
+            "fromUserId" => $userId,
+            "textMessage" => $textMessage,
+            "isDebug" => false
+        ));
+
+        $headers = array(
+            "Content-Type: application/json",
+            "Authorization: $token",
+            "Accept: application/json",
+            "Content-Length: ".strlen($data)
+        );
+
+        curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($request, CURLOPT_CUSTOMREQUEST, $service_method);
+        curl_setopt($request, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($request, CURLOPT_HTTPHEADER, $headers);
+        $curl_response = curl_exec($request);
+
+        $http_code = curl_getinfo($request, CURLINFO_HTTP_CODE);
+
+        if($http_code != 200){
+            throw new Exception("Wrong Chatbot Response.");
+        }
+
+        return json_decode($curl_response, true);
+    }
+
+    /**
+     * 检索机器人知识库
+     * @param $userId
+     * @param $query
+     * @return mixed
+     * @throws Exception
+     */
+    public function faq($userId, $query){
+        $service_path = '/api/v1/chatbot/' . $this->clientId . '/faq/query';
+        $service_url = $this->baseUrl . $service_path;
+        $service_method = "POST";
+        $request = curl_init($service_url);
+        $token = generate($this->clientId, $this->clientSecret, $service_method, $service_path);
+
+        $data = json_encode(array(
+            "fromUserId" => $userId,
+            "query" => $query
+        ));
+
+        $headers = array(
+            "Content-Type: application/json",
+            "Authorization: $token",
+            "Accept: application/json",
+            "Content-Length: ".strlen($data)
+        );
+
+        curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($request, CURLOPT_CUSTOMREQUEST, $service_method);
+        curl_setopt($request, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($request, CURLOPT_HTTPHEADER, $headers);
+        $curl_response = curl_exec($request);
+
+        $http_code = curl_getinfo($request, CURLINFO_HTTP_CODE);
+
+        if($http_code != 200){
+            throw new Exception("Wrong Chatbot Response.");
+        }
+
+        return json_decode($curl_response, true);
+    }
+
+
+    /**
+     * 查询用户列表
+     * @param int $limit
+     * @param int $page
+     * @param string $sortby
+     * @return mixed
+     * @throws Exception
+     */
+    public function users($limit = 50, $page = 1, $sortby = "-lasttime"){
+        $service_path = '/api/v1/chatbot/' . $this->clientId . '/users?page='.$page.'&limit='.$limit."&sortby=".$sortby;
+        $service_url = $this->baseUrl . $service_path;
+        $service_method = "GET";
+        $request = curl_init($service_url);
+        $token = generate($this->clientId, $this->clientSecret, $service_method, $service_path);
+
+        $headers = array(
+            "Content-Type: application/json",
+            "Authorization: $token",
+            "Accept: application/json"
+        );
+
+        curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($request, CURLOPT_CUSTOMREQUEST, $service_method);
+        curl_setopt($request, CURLOPT_HTTPHEADER, $headers);
+        $curl_response = curl_exec($request);
+
+        $http_code = curl_getinfo($request, CURLINFO_HTTP_CODE);
+
+        if($http_code != 200){
+            throw new Exception("Wrong Chatbot Response.");
+        }
+
+        return $this->purge(json_decode($curl_response, true));
+    }
+
+
+    /**
+     * 查看一个用户的聊天历史
+     * @param $userId
+     * @param int $limit
+     * @param int $page
+     * @param string $sortby
+     * @return mixed
+     * @throws Exception
+     */
+    public function chats($userId, $limit = 50, $page = 1, $sortby = '-lasttime'){
+        $service_path = '/api/v1/chatbot/' . $this->clientId . '/users/'.$userId.'/chats?page='.$page.'&limit='.$limit."&sortby=".$sortby;
+        $service_url = $this->baseUrl . $service_path;
+        $service_method = "GET";
+        $request = curl_init($service_url);
+        $token = generate($this->clientId, $this->clientSecret, $service_method, $service_path);
+
+        $headers = array(
+            "Content-Type: application/json",
+            "Authorization: $token",
+            "Accept: application/json"
+        );
+
+        curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($request, CURLOPT_CUSTOMREQUEST, $service_method);
+        curl_setopt($request, CURLOPT_HTTPHEADER, $headers);
+        $curl_response = curl_exec($request);
+
+        $http_code = curl_getinfo($request, CURLINFO_HTTP_CODE);
+
+        if($http_code != 200){
+            throw new Exception("Wrong Chatbot Response.");
+        }
+
+        return $this->purge(json_decode($curl_response, true));
+    }
+
+    /**
+     * 屏蔽用户
+     * @param $userId
+     * @return bool
+     * @throws Exception
+     */
+    public function mute($userId){
+        $service_path = '/api/v1/chatbot/' . $this->clientId . '/users/'.$userId.'/mute';
+        $service_url = $this->baseUrl . $service_path;
+        $service_method = "POST";
+        $request = curl_init($service_url);
+        $token = generate($this->clientId, $this->clientSecret, $service_method, $service_path);
+
+        $headers = array(
+            "Content-Type: application/json",
+            "Authorization: $token",
+            "Accept: application/json"
+        );
+
+        curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($request, CURLOPT_CUSTOMREQUEST, $service_method);
+        curl_setopt($request, CURLOPT_HTTPHEADER, $headers);
+        $curl_response = curl_exec($request);
+
+        $http_code = curl_getinfo($request, CURLINFO_HTTP_CODE);
+
+        if($http_code != 200){
+            throw new Exception("Wrong Chatbot Response.");
+        }
+
+        $result = json_decode($curl_response, true);
+
+        if(isset($result['rc']) && ($result['rc'] == 0)){
+            return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * 取消屏蔽用户
+     * @param $userId
+     * @return bool
+     * @throws Exception
+     */
+    public function unmute($userId){
+        $service_path = '/api/v1/chatbot/' . $this->clientId . '/users/'.$userId.'/unmute';
+        $service_url = $this->baseUrl . $service_path;
+        $service_method = "POST";
+        $request = curl_init($service_url);
+        $token = generate($this->clientId, $this->clientSecret, $service_method, $service_path);
+
+        $headers = array(
+            "Content-Type: application/json",
+            "Authorization: $token",
+            "Accept: application/json"
+        );
+
+        curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($request, CURLOPT_CUSTOMREQUEST, $service_method);
+        curl_setopt($request, CURLOPT_HTTPHEADER, $headers);
+        $curl_response = curl_exec($request);
+
+        $http_code = curl_getinfo($request, CURLINFO_HTTP_CODE);
+
+        if($http_code != 200){
+            throw new Exception("Wrong Chatbot Response.");
+        }
+
+        $result = json_decode($curl_response, true);
+
+        if(isset($result['rc']) && ($result['rc'] == 0)){
+            return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * 检测用户是否被屏蔽
+     * @param $userId
+     * @return mixed
+     * @throws Exception
+     */
+    public function ismute($userId){
+        $service_path = '/api/v1/chatbot/' . $this->clientId . '/users/'.$userId.'/ismute';
+        $service_url = $this->baseUrl . $service_path;
+        $service_method = "POST";
+        $request = curl_init($service_url);
+        $token = generate($this->clientId, $this->clientSecret, $service_method, $service_path);
+
+        $headers = array(
+            "Content-Type: application/json",
+            "Authorization: $token",
+            "Accept: application/json"
+        );
+
+        curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($request, CURLOPT_CUSTOMREQUEST, $service_method);
+        curl_setopt($request, CURLOPT_HTTPHEADER, $headers);
+        $curl_response = curl_exec($request);
+
+        $http_code = curl_getinfo($request, CURLINFO_HTTP_CODE);
+
+        if($http_code != 200){
+            throw new Exception("Wrong Chatbot Response.");
+        }
+
+        $result = json_decode($curl_response, true);
+
+
+        if(isset($result['rc']) && ($result['rc'] == 0)){
+            return $result['data']['mute'];
+        }
+        throw new Exception("Unexpected Chatbot Response.");
+    }
+
+    /**
+     * 读取用户画像
+     * @param $userId
+     * @return mixed
+     * @throws Exception
+     */
+    public function profile($userId){
+        $service_path = '/api/v1/chatbot/' . $this->clientId . '/users/'.$userId.'/profile';
+        $service_url = $this->baseUrl . $service_path;
+        $service_method = "POST";
+        $request = curl_init($service_url);
+        $token = generate($this->clientId, $this->clientSecret, $service_method, $service_path);
+
+        $headers = array(
+            "Content-Type: application/json",
+            "Authorization: $token",
+            "Accept: application/json"
+        );
+
+        curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($request, CURLOPT_CUSTOMREQUEST, $service_method);
+        curl_setopt($request, CURLOPT_HTTPHEADER, $headers);
+        $curl_response = curl_exec($request);
+
+        $http_code = curl_getinfo($request, CURLINFO_HTTP_CODE);
+
+        if($http_code != 200){
+            throw new Exception("Wrong Chatbot Response.");
+        }
+
+        return json_decode($curl_response, true);
+    }
+
+
+    /**
+     * 删除内部ID
+     * @param $resp
+     * @return mixed
+     */
+    private function purge($resp){
+        if(isset($resp["data"]) && is_array($resp["data"])){
+            foreach ($resp["data"] as $key => $value){
+                // data: sublist
+                if(is_array($resp["data"][$key])){
+                    foreach ($resp["data"][$key] as $key2 => $value2){
+                        if($key2 == "chatbotID")
+                            unset($resp["data"][$key][$key2]);
+                    }
+                } else { // data: plain object
+                    if($key == "chatbotID"){
+                        unset($resp["data"][$key]);
+                    }
+                }
+            }
+        }
+        return $resp;
+    }
+}
--- test/ChatbotTest.php
+++ test/ChatbotTest.php
@@ -6,7 +6,7 @@ include_once __DIR__ . "/../src/chatopera/sdk.php";
 use PHPUnit\Framework\TestCase;
 use chatopera\sdk\Chatbot;
 
-final class EmailTest extends TestCase{
+final class ChatbotTest extends TestCase{
 
     public function testGenerateCredentails(){
         $appId = "5bf27e4d6f80090017b404b7";
@@ -16,16 +16,106 @@ final class EmailTest extends TestCase{
         chatopera\sdk\generate($appId, $secret, $method, $path);
     }
 
-    public function testCanBeCreatedFromValidEmailAddress()
+    public function testChatbotDetail()
     {
         $appId = "5bf27e4d6f80090017b404b7";
         $secret = "e4cbc6a65708c011ec0da73b0f5db7a1";
-        $method = "POST";
-        $path = "/api/v1/chatbot/".$appId."/faq/query";
         $chatbot = new Chatbot($appId, $secret);
-
-        $chatbot->print();
+        $resp = $chatbot->detail();
+        print_r($resp);
+        if(isset($resp['rc'])  && ($resp['rc'] == 0)){
+            print "done";
+        } else {
+             throw new Exception("wrong response.");
+        }
     }
+
+    public function testChatbotConversation()
+    {
+        $appId = "5bf27e4d6f80090017b404b7";
+        $secret = "e4cbc6a65708c011ec0da73b0f5db7a1";
+        $chatbot = new Chatbot($appId, $secret);
+        $resp = $chatbot->conversation("phpsdk", "你好");
+        print_r($resp);
+        if(isset($resp['rc'])  && ($resp['rc'] == 0)){
+            print "pass";
+        } else {
+            throw new Exception("wrong response.");
+        }
+    }
+
+    public function testChatbotFaq()
+    {
+        $appId = "5bf27e4d6f80090017b404b7";
+        $secret = "e4cbc6a65708c011ec0da73b0f5db7a1";
+        $chatbot = new Chatbot($appId, $secret);
+        $resp = $chatbot->faq("phpsdk", "机器人怎么购买");
+        print_r($resp);
+        if(isset($resp['rc'])  && ($resp['rc'] == 0)){
+            print "pass";
+        } else {
+            throw new Exception("wrong response.");
+        }
+    }
+
+    public function testChatbotUsers()
+    {
+        $appId = "5bf27e4d6f80090017b404b7";
+        $secret = "e4cbc6a65708c011ec0da73b0f5db7a1";
+        $chatbot = new Chatbot($appId, $secret);
+        $resp = $chatbot->users(50, 1);
+        print_r($resp);
+        if(isset($resp['rc'])  && ($resp['rc'] == 0)){
+            print "pass";
+        } else {
+            throw new Exception("wrong response.");
+        }
+    }
+
+
+    public function testChatbotUserChats()
+    {
+        $appId = "5bf27e4d6f80090017b404b7";
+        $secret = "e4cbc6a65708c011ec0da73b0f5db7a1";
+        $chatbot = new Chatbot($appId, $secret);
+        $resp = $chatbot->chats('phpsdk', 10);
+        print_r($resp);
+        if(isset($resp['rc'])  && ($resp['rc'] == 0)){
+            print "pass";
+        } else {
+            throw new Exception("wrong response.");
+        }
+    }
+
+//    public function testChatbotMute()
+//    {
+//        $appId = "5bf27e4d6f80090017b404b7";
+//        $secret = "e4cbc6a65708c011ec0da73b0f5db7a1";
+//        $chatbot = new Chatbot($appId, $secret);
+//        $resp = $chatbot->mute('phpsdk');
+//        print_r($resp);
+//    }
+
+//    public function testChatbotIsMute()
+//    {
+//        $appId = "5bf27e4d6f80090017b404b7";
+//        $secret = "e4cbc6a65708c011ec0da73b0f5db7a1";
+//        $chatbot = new Chatbot($appId, $secret);
+//        $resp = $chatbot->ismute('phpsdk');
+//        print($resp ? "true":"false");
+//    }
+
+
+    public function testChatbotUserProfile()
+    {
+        $appId = "5bf27e4d6f80090017b404b7";
+        $secret = "e4cbc6a65708c011ec0da73b0f5db7a1";
+        $chatbot = new Chatbot($appId, $secret);
+        $resp = $chatbot->profile('phpsdk');
+        print_r($resp);
+    }
+
+
 }
 
 
