91714f01dabcd2e5bc5a2515ac999c69d1c60e0b
Hai Liang Wang <hailiang.hl.wang@gmail.com>
2018-11-20 15:29:12 +0800

    Update version

--- test/ChatbotTest.php
+++ test/ChatbotTest.php
@@ -7,6 +7,15 @@ use PHPUnit\Framework\TestCase;
 use chatopera\sdk\Chatbot;
 
 final class EmailTest extends TestCase{
+
+    public function testGenerateCredentails(){
+        $appId = "5bf27e4d6f80090017b404b7";
+        $secret = "e4cbc6a65708c011ec0da73b0f5db7a1";
+        $method = "POST";
+        $path = "/api/v1/chatbot/".$appId."/faq/query";
+        chatopera\sdk\generate($appId, $secret, $method, $path);
+    }
+
     public function testCanBeCreatedFromValidEmailAddress()
     {
         $appId = "5bf27e4d6f80090017b404b7";
